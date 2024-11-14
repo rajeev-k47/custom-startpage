7407c7ec8a75ee320ca7bfc127220a8f4b56891c
rajeev-k47 <164878192+rajeev-k47@users.noreply.github.com>
2024-11-15 02:04:53 +0530

    <*/*>?

--- js/config.js
+++ js/config.js
@@ -57,8 +57,11 @@ class Config {
 			'find/': {
 				urlPrefix: 'https://ewasion.github.io/opendirectory-finder/'
 			},
-			'm/': {
-				urlPrefix: 'https://mail.google.com/u/'
+			'm/1': {
+				urlPrefix: 'https://mail.google.com/u/1'
+			},
+			'm/0': {
+				urlPrefix: 'https://mail.google.com/u/0'
 			}
 		};
 
